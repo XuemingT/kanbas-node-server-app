b3639535b2477719aa4fc8d22830d028eef07e85
XuemingT <xmtang810@gmail.com>
2024-12-06 16:55:06 -0800

    fixagain

--- index.js
+++ index.js
@@ -30,11 +30,6 @@ const sessionOptions = {
 };
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.NODE_SERVER_DOMAIN,
-  };
 }
 app.use(session(sessionOptions));
 

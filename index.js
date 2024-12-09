02b164c7d8227840f7e7d465a695e54d1e4d86c7
XuemingT <xmtang810@gmail.com>
2024-12-08 22:35:08 -0800

    session

--- index.js
+++ index.js
@@ -3,36 +3,43 @@ import Hello from "./Hello.js";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import "dotenv/config";
 import session from "express-session";
 import "dotenv/config";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import courses from "./Kanbas/Database/courses.js";
 import QuizRoutes from "./Kanbas/Quizzes/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-
+import mongoose from "mongoose";
 import users from "./Kanbas/Database/users.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    origin: [
+      "http://localhost:3000" ||
+        "https://kanbas-react-web-app-cs5610-a6.netlify.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
-};
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
+  cookie: {
     sameSite: "none",
     secure: true,
-    domain: process.env.NODE_SERVER_DOMAIN,
-  };
-}
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  },
+  proxy: true,
+};
 app.use(session(sessionOptions));
 
 app.use(express.json());
